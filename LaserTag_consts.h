abbd6d9636fed8b3a06b7da11150a0aabb5757e9
Diego Marvid <diegomarvid99@hotmail.com>
2021-07-22 23:00:08 -0300

    Audios + weapon size consts in weapon consts header

--- LaserTag_consts.h
+++ LaserTag_consts.h
@@ -12,9 +12,5 @@ const int TEAM_AMOUNT = 4;
 
 const char Teams[TEAM_AMOUNT] = {WHITE_TEAM, BLUE_TEAM, RED_TEAM, GREEN_TEAM};
 
-const int WEAPONS_AMOUNT = 2;
-
-const char Weapons[WEAPONS_AMOUNT] = {REVOLVER_ID, ROCKET_LAUNCHER_ID};
-
 
 #endif
--- Weapons_consts.h
+++ Weapons_consts.h
@@ -5,4 +5,8 @@ const char REVOLVER_ID = 'K';
 
 const char ROCKET_LAUNCHER_ID = 'R';
 
+const int WEAPONS_AMOUNT = 2;
+
+const char Weapons[WEAPONS_AMOUNT] = {REVOLVER_ID, ROCKET_LAUNCHER_ID};
+
 #endif
