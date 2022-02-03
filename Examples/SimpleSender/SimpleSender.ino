a59427871c491e56b3206578d96f51dc337eefc3
Diego Marvid <33181424+diegomarvid@users.noreply.github.com>
2022-02-02 21:02:47 -0300

    Update SimpleSender example

--- Examples/SimpleSender/SimpleSender.ino
+++ Examples/SimpleSender/SimpleSender.ino
@@ -1,32 +1,21 @@
 #include <LaserTag.h>
-#include <Button.h>
-
-const bool PRESSED = 1;
 
 const int TX_SIGNAL_PIN = 8;
-const int R_LED_PIN = 9;
-const int G_LED_PIN = 10;
-const int B_LED_PIN = 11;
 
-const char PLAYER_ID = 'D';
+const char PLAYER_ID = 'A';
 
 LaserTag laserTag(RED_TEAM, PLAYER_ID);
 
-Button TriggerButton(4);
-Button ReloadButton(5);
-Button ChangeWeaponButton(6);
-Button ChangeTeamButton(7);
+Button TriggerButton(18);
+Button ReloadButton(26);
 
 void setup() {
   
-  Serial.begin(9600);
+  Serial.begin(115200);
 
   pinMode(TX_SIGNAL_PIN, OUTPUT);
-  pinMode(R_LED_PIN, OUTPUT);
-  pinMode(G_LED_PIN, OUTPUT);
-  pinMode(B_LED_PIN, OUTPUT);
 
-  SetLed(0,0,0);
+  laserTag.SetWifiCredentials("Ricciardi", "ricciardi");
 
   laserTag.SetIRTransmitterPin(TX_SIGNAL_PIN);
   laserTag.SetInterruptCallback(Callback);
@@ -41,18 +30,20 @@ void Callback()
   laserTag.HandleInterrupt();
 }
 
-void SetLed(int r, int g, int b)
-{
-  analogWrite(R_LED_PIN, 255 - r );
-  analogWrite(G_LED_PIN, 255 - g );
-  analogWrite(B_LED_PIN, 255 - b);
+void onConnectionEstablished() {
+
+  laserTag.HandleMQTTConnection();
+  
 }
 
-
-void loop() {
+void loop() 
+{
+  
+  laserTag.MQTTLoop();
 
   if(TriggerButton.IsReleased())
   {
+    Serial.println("Shooting!");
     laserTag.Shoot();
   }
 
@@ -61,47 +52,5 @@ void loop() {
     laserTag.Reload();
   }
 
-  if(ChangeWeaponButton.IsReleased())
-  {
-
-    laserTag.ChangeToNextWeapon();
-    Serial.print("Changed weapon to: ");
-    Serial.println(laserTag.GetCurrentWeapon());
-    
-  }
-
-
-  if(ChangeTeamButton.IsReleased())
-  {
-
-    laserTag.ChangeToNextTeam();
-    //Serial.print("Changed team to: ");
-    //Serial.println(laserTag.GetCurrentTeam());
-
-    if(laserTag.GetCurrentTeam() == WHITE_TEAM)
-    {
-      SetLed(255,255,255);
-      Serial.println(laserTag.GetCurrentTeam());
-    }
-
-    if(laserTag.GetCurrentTeam() == BLUE_TEAM)
-    {
-      SetLed(0,0,255);
-      Serial.println(laserTag.GetCurrentTeam());
-    }
-
-    if(laserTag.GetCurrentTeam() == RED_TEAM)
-    {
-      SetLed(255,0,0);
-      Serial.println(laserTag.GetCurrentTeam());
-    }
-
-      if(laserTag.GetCurrentTeam() == GREEN_TEAM)
-    {
-      SetLed(0,255,0);
-      Serial.println(laserTag.GetCurrentTeam());
-    }
-    
-  }
- 
+     
 }
